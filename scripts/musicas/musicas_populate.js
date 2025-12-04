34678d6732a69eda4b4bfd9b6c133640d778a3d9
Ian-Cerqueira <168128013+Ian-Cerqueira@users.noreply.github.com>
2025-12-04 22:57:59 +0000

    musica_final expots

--- scripts/musicas/musicas_populate.js
+++ scripts/musicas/musicas_populate.js
@@ -1,10 +1,12 @@
 import { musicasf } from "./musicas_array.js";
 
-let musicas_final = musicasf;
+export let musicas_final = musicasf;
 let counter = 1;
 musicas_final.forEach(element => {
     element['musica_id'] = counter;
     counter = counter + 1;
 });
 
-console.log(JSON.stringify(musicas_final));
+db.musicas.insertMany(musicas_final);
+
+//console.log(JSON.stringify(musicas_final));
