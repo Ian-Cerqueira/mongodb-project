7c569edbf36f18fd5ea4fadbc8410bc4bff3b351
Ian-Cerqueira <168128013+Ian-Cerqueira@users.noreply.github.com>
2025-12-04 23:11:57 +0000

    Adiciona id nos artistas

---
+++ scripts/artistas/artistas_populate.js
@@ -0,0 +1,10 @@
+import { artistas_f } from "./artistas_array.js";
+
+export let artistas_final = artistas_f;
+let counter = 1;
+artistas_final.forEach(element => {
+    element['artista_id'] = counter;
+    counter = counter + 1;
+});
+
+db.artistas.insertMany(artistas_final);
